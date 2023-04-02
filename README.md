6d40d69d48117b87faacd5ebc1827c47a4f7c939
woobinpro <woobinpro@gmail.com>
2023-04-03 03:16:19 +0900

    generate token.json

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sending_Email_Gmail-API
+
+The following Link is guide<br/>
+https://www.labnol.org/google-api-service-account-220405
---
+++ token.js
@@ -0,0 +1,17 @@
+// token.js
+
+const { google } = require('googleapis');
+const path = require('path');
+const fs = require('fs');
+const credentials = require('./credentials.json');
+
+// Replace with the code you received from Google
+const code = '4/0AVHEtk6_jn8bnYD07vLxO0_DMEn0-jIw2whYECzWZb1o4x3mYXake_LW-76677_1CVNrnQ';
+const { client_secret, client_id, redirect_uris } = credentials.installed;
+const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+oAuth2Client.getToken(code).then(({ tokens }) => {
+  const tokenPath = path.join(__dirname, 'token.json');
+  fs.writeFileSync(tokenPath, JSON.stringify(tokens));
+  console.log('Access token and refresh token stored to token.json');
+});
